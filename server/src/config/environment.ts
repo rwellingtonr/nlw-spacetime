1ba2a8d3c912be48316dbd239eb45815e93cbdd4
Wellington <wellington-158@hotmail.com>
2023-06-13 20:36:27 -0300

    feat: variáveis de ambiente

--- server/src/config/environment.ts
+++ server/src/config/environment.ts
@@ -3,6 +3,10 @@ import { z } from "zod"
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
+  DATABASE_URL: z.string(),
+  GITHUB_CLIENT_SECRET: z.string().min(10),
+  GITHUB_CLIENT_ID: z.string().min(6),
+  JWT_SECRET: z.string().min(8),
 })
 
 const env = envSchema.safeParse(process.env)
@@ -11,4 +15,8 @@ if (!env.success) {
 }
 export const environment = {
   port: env.data.PORT,
+  database: env.data.DATABASE_URL,
+  githubClientSecret: env.data.GITHUB_CLIENT_SECRET,
+  githubClientId: env.data.GITHUB_CLIENT_ID,
+  jwtSecret: env.data.JWT_SECRET,
 }
