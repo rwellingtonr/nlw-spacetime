5a3282b15af618c2712b4d2fdb566c14f1032e9e
Wellington <wellington-158@hotmail.com>
2023-06-13 20:37:54 -0300

    feat: gerenciamento de rotas

--- server/src/infra/http/routes/index.ts
+++ server/src/infra/http/routes/index.ts
@@ -1,9 +1,2 @@
-import { FastifyInstance } from "fastify"
-import * as controllers from "../controller"
-
-export function handleRoutes(app: FastifyInstance) {
-  app.get("/memories", controllers.handleListMemories)
-  app.post("/memories", controllers.handleCreateMemory)
-  app.get("/memories/:id", controllers.handleFindMemories)
-  app.put("/memories/:id", controllers.handleUpdateMemories)
-}
+export * from "./memories"
+export * from "./authorization"
