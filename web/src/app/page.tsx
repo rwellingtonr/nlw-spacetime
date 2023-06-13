a8c4a5375c46245a7456ebf2ee7ddb003ae35080
Wellington <wellington-158@hotmail.com>
2023-06-13 20:35:58 -0300

    feat: redirect

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,5 @@
-import { Blur, Footer, Hero, Stripes } from "@/Components";
+import { Blur, Footer, Hero, Stripes } from "@/components";
+import { EmptyMemories } from "@/components/EmptyMemories";
 import { User } from "lucide-react";
 
 export default function Home() {
@@ -11,7 +12,7 @@ export default function Home() {
         <Stripes />
 
         <a
-          href="#"
+          href={`https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}
           className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
         >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
@@ -30,17 +31,7 @@ export default function Home() {
       </div>
       {/* Left */}
       <div className="flex flex-col bg-[url(../assets/stars.svg)] bg-cover p-16">
-        <div className="flex flex-1 items-center justify-center">
-          <p className="w-[22.5rem] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{" "}
-            <a
-              href="#"
-              className="underline  transition-colors  hover:text-gray-50"
-            >
-              criar agora!
-            </a>
-          </p>
-        </div>
+       <EmptyMemories />
       </div>
     </main>
   );
